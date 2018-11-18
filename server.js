b3e5ee2b1ee9dbabe748eb57873f662ef2828d32
Farza Nurifan <farzanurifan@gmail.com>
2018-11-18 12:54:07 +0700

    CRUD

--- server.js
+++ server.js
@@ -1,37 +1,68 @@
-const express = require('express');
 const bodyParser = require('body-parser')
-const app = express();
+const methodOverride = require('method-override')
+const express = require('express')
 const MongoClient = require('mongodb').MongoClient
-app.use(bodyParser.urlencoded({ extended: true }))
+const ObjectId = require('mongodb').ObjectID;
 
+const app = express()
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+app.set('view engine', 'ejs')
 
 var db
 var uri = 'mongodb+srv://farzanurifan:bismillah@bdt-6ij3v.mongodb.net/test'
-
-MongoClient.connect(uri, (err, client) => {
+MongoClient.connect(uri, { useNewUrlParser: true }, (err, client) => {
     if (err) return console.log(err)
     db = client.db('nba')
+})
 
-    app.listen(3000, () => {
-        console.log('listening on 3000')
-    })
+app.listen(3000, () => {
+    console.log('listening on 3000')
+})
 
-    app.get('/', (req, res) => {
-        db.collection('quotes').find().toArray(function (err, results) {
-            console.log(results)
-            // send HTML file populated with quotes here
-            res.sendFile(__dirname + '/index.html')
-        })
-    })
-
-    app.post('/quotes', (req, res) => {
-        console.log(req.body)
-        db.collection('quotes').save(req.body, (err, result) => {
-            if (err) return console.log(err)
-
-            console.log('saved to database')
-            res.redirect('/')
-        })
+app.get('/', (req, res) => {
+    db.collection('quotes').find().toArray((err, results) => {
+        res.render('index.ejs', { quotes: results })
     })
 })
 
+app.get('/add', (req, res) => {
+    res.render('add.ejs')
+})
+
+app.post('/create', (req, res) => {
+    db.collection('quotes').save(req.body, (err, result) => {
+        if (err) return console.log(err)
+
+        console.log('saved to database')
+        res.redirect('/')
+    })
+})
+
+app.get('/edit/:id', (req, res) => {
+    var id = ObjectId(req.params.id)
+    db.collection('quotes').find(id).toArray((err, results) => {
+        result = results[0]
+        res.render('edit.ejs', { quote: result })
+    })
+})
+
+app.put('/update/:id', (req, res) => {
+    var id = ObjectId(req.params.id)
+    db.collection('quotes').updateOne({ _id: id }, { $set: req.body }, (err, result) => {
+        if (err) return console.log(err)
+
+        console.log('updated to database')
+        res.redirect('/')
+    })
+})
+
+app.delete('/delete/:id', (req, res) => {
+    var id = ObjectId(req.params.id)
+    db.collection('quotes').deleteOne({ _id: id }, (err, result) => {
+        if (err) return console.log(err)
+
+        console.log('deleted from database')
+        res.redirect('/')
+    })
+})
