51ab480c4e7d8cfbb1dc9bcace7eccd15cd61626
Farza Nurifan <farzanurifan@gmail.com>
2018-11-18 15:54:51 +0700

    Fixes

--- server.js
+++ server.js
@@ -4,20 +4,31 @@ const express = require('express')
 const MongoClient = require('mongodb').MongoClient
 const ObjectId = require('mongodb').ObjectID;
 
+const logError = (err) => {
+    if (err) return console.log(err)
+}
+
+const logMessage = (message) => {
+    console.log(message)
+}
+
 const app = express()
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.set('view engine', 'ejs')
 
+const database = 'nba'
+const table = 'quotes'
+
 var db
 var uri = 'mongodb+srv://farzanurifan:bismillah@bdt-6ij3v.mongodb.net/test'
 MongoClient.connect(uri, { useNewUrlParser: true }, (err, client) => {
-    if (err) return console.log(err)
-    db = client.db('nba')
+    logError(err)
+    db = client.db(database)
 })
 
 app.listen(3000, () => {
-    console.log('listening on 3000')
+    logMessage('listening on 3000')
 })
 
 app.get('/', (req, res) => {
@@ -25,12 +36,11 @@ app.get('/', (req, res) => {
 })
 
 app.get('/page/:page', (req, res) => {
-    let alldata = 0
     var page = Number(req.params.page)
-    db.collection('quotes').find().toArray((err, results) => {
-        alldata = results.length
-        db.collection('quotes').find().skip(10 * (page - 1)).limit(10).toArray((err, results) => {
-            res.render('index.ejs', { quotes: results, page, alldata })
+    db.collection(table).find().toArray((err, results) => {
+        var alldata = results.length
+        db.collection(table).find().skip(10 * (page - 1)).limit(10).toArray((err, results) => {
+            res.render('index.ejs', { results, page, alldata })
         })
     })
 })
@@ -40,38 +50,35 @@ app.get('/add', (req, res) => {
 })
 
 app.post('/create', (req, res) => {
-    db.collection('quotes').save(req.body, (err, result) => {
-        if (err) return console.log(err)
-
-        console.log('saved to database')
+    db.collection(table).save(req.body, (err, result) => {
+        logError(err)
+        logMessage('saved to database')
         res.redirect('/')
     })
 })
 
 app.get('/edit/:id', (req, res) => {
     var id = ObjectId(req.params.id)
-    db.collection('quotes').find(id).toArray((err, results) => {
+    db.collection(table).find(id).toArray((err, results) => {
         result = results[0]
-        res.render('edit.ejs', { quote: result })
+        res.render('edit.ejs', { result })
     })
 })
 
 app.put('/update/:id', (req, res) => {
     var id = ObjectId(req.params.id)
-    db.collection('quotes').updateOne({ _id: id }, { $set: req.body }, (err, result) => {
-        if (err) return console.log(err)
-
-        console.log('updated to database')
+    db.collection(table).updateOne({ _id: id }, { $set: req.body }, (err, result) => {
+        logError(err)
+        logMessage('updated to database')
         res.redirect('/')
     })
 })
 
 app.delete('/delete/:id', (req, res) => {
     var id = ObjectId(req.params.id)
-    db.collection('quotes').deleteOne({ _id: id }, (err, result) => {
-        if (err) return console.log(err)
-
-        console.log('deleted from database')
+    db.collection(table).deleteOne({ _id: id }, (err, result) => {
+        logError(err)
+        logMessage('deleted from database')
         res.redirect('/')
     })
 })
